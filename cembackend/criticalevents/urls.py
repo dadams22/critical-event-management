53b52c93ea5530037d44d482a74caa6340e8c38e
David Adams <dadams2212@gmail.com>
2023-06-21 01:31:36 -0700

    Add rest framework token auth

--- cembackend/criticalevents/urls.py
+++ cembackend/criticalevents/urls.py
@@ -1,11 +1,13 @@
 from django.urls import path, include
 from rest_framework import routers
+from rest_framework.authtoken import views
 
 from .views import MessageView
 
 router = routers.SimpleRouter()
 
 urlpatterns = [
+    path('auth', views.obtain_auth_token),
     path('message', MessageView.as_view()),
     path('', include(router.urls)),
 ]
