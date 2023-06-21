22e313a7b91b37825fd41988a27389536bdb994e
David Adams <dadams2212@gmail.com>
2023-06-21 00:59:11 -0700

    Add Person model, create migration, and register with admin site

--- cembackend/criticalevents/models.py
+++ cembackend/criticalevents/models.py
@@ -1,3 +1,11 @@
 from django.db import models
 
-# Create your models here.
+
+class Person(models.Model):
+    """ Represents a person who may be impacted by an incident """
+    first_name = models.CharField(max_length=50)
+    last_name = models.CharField(max_length=50)
+    phone = models.CharField(max_length=50)
+
+    def __str__(self) -> str:
+        return f'{self.first_name} {self.last_name}'
--- cembackend/criticalevents/views.py
+++ cembackend/criticalevents/views.py
@@ -2,13 +2,14 @@ from rest_framework.views import APIView
 from rest_framework.response import Response
 from rest_framework import status
 
+from .models import Person
 from .twilio_utls import send_twilio_message
 
 class MessageView(APIView):
     def post(self, request):
-        # Perform any desired logic here
-        # Access request data using `request.data`
+        recipients = Person.objects.all()
 
-        send_twilio_message('+18608172974', 'Hello, world!')
+        for recipient in recipients:
+            send_twilio_message(recipient.phone, f'TWILIO TEST: Hello, {recipient.first_name} {recipient.last_name}. This is a test message.')
         
         return Response({"message": "Message sent successfully"}, status=status.HTTP_200_OK)
