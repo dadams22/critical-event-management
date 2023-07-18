5c40d76708ae016a28f8aebed548a47e20b92147
David Adams <dadams2212@gmail.com>
2023-07-17 22:58:24 -0700

    Add message templates

--- cemfrontend/app/(modals)/SendAlertModal.tsx
+++ cemfrontend/app/(modals)/SendAlertModal.tsx
@@ -1,15 +1,32 @@
 'use client';
 
-import { Button, Stack, Textarea } from '@mantine/core';
+import { Button, NativeSelect, Select, SelectItem, Stack, Textarea } from '@mantine/core';
 import { ContextModalProps } from '@mantine/modals';
 import { IconSpeakerphone } from '@tabler/icons';
+import { useState } from 'react';
+
+const MESSAGE_TEMPLATES: SelectItem[] = [
+    { label: 'Fire', value: 'FIRE: A fire has been reported in the building. Please proceed to the nearest exit in an orderly manner.' },
+    { label: 'Tornado', value: 'TORNADO: A tornado warning has been put in place. Please proceed to the first floor and shelter in place. Avoid any windows and use the desks and tables as shelter.' },
+]
 
 interface ComponentProps {}
 
 export default function SendAlertModal({}: ContextModalProps<ComponentProps>) {
+    const [message, setMessage] = useState<string>('');
+
+    const handleSelectTemplate = (value: string | null) => {
+        setMessage(value || '');
+    };
+
+    const handleMessageChange = (event: React.ChangeEvent<HTMLTextAreaElement>) => {
+        setMessage(event.target.value);
+    };
+
 	return (
 		<Stack>
-			<Textarea label="Message" placeholder="Message..." withAsterisk />
+            <Select label="Template" placeholder="No template selected" data={MESSAGE_TEMPLATES} onChange={handleSelectTemplate} />
+			<Textarea label="Message" value={message} onChange={handleMessageChange} placeholder="Message..." minRows={5} withAsterisk />
 			<Button color="red" leftIcon={<IconSpeakerphone size={20} />}>
 				Send Alert
 			</Button>
