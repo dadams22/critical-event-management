15e84fb53a1e059d3427a87b763fb313549fcc0e
Mike Lyons <mdl0394@gmail.com>
2024-04-23 16:59:50 -0700

    b: change asserted status code in test_smoke ping test

--- cembackend/criticalevents/tests/test_smoke.py
+++ cembackend/criticalevents/tests/test_smoke.py
@@ -3,4 +3,4 @@ from django.test import LiveServerTestCase
 class PingTestCase(LiveServerTestCase):
     def test_ping(self):
         response = self.client.get('/api/ping')
-        self.assertEqual(response.json(), { 'message': 'pong' })
+        self.assertEqual(response.status_code, 200)
